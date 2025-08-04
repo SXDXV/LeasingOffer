0c99813cf700365495a4c47f3b64fb192ced8679
Dmitry <99351480+SXDXV@users.noreply.github.com>
2025-08-04 13:22:49 +0300

    Update Program.cs
    Remove broken encoding comments

--- LeasingOfferBack/Program.cs
+++ LeasingOfferBack/Program.cs
@@ -5,18 +5,15 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// контроллеры
 builder.Services.AddControllers();
 
 // Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Разрешаем CORS (на будущее для фронта)
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
